780decf2670db9c8253f444e07be432e55fbf161
mfkrdmn <muhammetfatihkaraduman@gmail.com>
2022-11-12 10:41:34 +0300

    cart setup processed

--- project/store/urls.py
+++ project/store/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('store', views.store, name = 'store'),
     path('store/<slug:category_slug>/', views.store, name = 'products_by_category'),
-    path('store/<slug:category_slug>/<slug:product_slug>/', views.product_detail, name = 'product_detail'),
+    path('store/<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
 ]
 
 
