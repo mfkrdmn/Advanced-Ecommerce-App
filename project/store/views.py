43e7f03272cbc83a2dd8510003be5d9484fdc25b
mfkrdmn <muhammetfatihkaraduman@gmail.com>
2022-11-12 13:13:03 +0300

    cart functionalities done

--- project/store/views.py
+++ project/store/views.py
@@ -1,6 +1,8 @@
 from category.models import Category
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, redirect, render
+from carts.views import _cart_id
+from carts.models import *
 
 from .models import *
 
@@ -19,7 +21,7 @@ def store(request, category_slug=None):
         product_count = products.count()
     else:
         products = Product.objects.all().filter(is_available=True).order_by('id')
-        paginator = Paginator(products, 4)
+        paginator = Paginator(products, 8)
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
         product_count = products.count()
@@ -35,7 +37,8 @@ def store(request, category_slug=None):
 def product_detail(request, category_slug, product_slug):
 
     single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
-        # in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
+    in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
+    
 
 
     # if request.user.is_authenticated:
@@ -54,7 +57,7 @@ def product_detail(request, category_slug, product_slug):
 
     context = {
         'single_product': single_product,
-        # 'in_cart'       : in_cart,
+        'in_cart'       : in_cart,
         # 'orderproduct': orderproduct,
         # 'reviews': reviews,
         # 'product_gallery': product_gallery,
